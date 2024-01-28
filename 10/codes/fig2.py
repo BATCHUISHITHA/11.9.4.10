0824b1316ecf4750ff81d7f3727e110b1d6b35f0
BATCHUISHITHA <155352743+BATCHUISHITHA@users.noreply.github.com>
2024-01-29 00:42:22 +0530

    Add files via upload

--- 10/codes/fig2.py
+++ 10/codes/fig2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 # Define the sequence function
 def sequence(n):
-    return (2 * n + 1) ** 2
+    return (2 * n - 1) ** 2
 
 # Generate x values (e.g., n values)
 x_values = range(0, 15)
@@ -12,9 +12,9 @@ y_values = [sequence(n) for n in x_values]
 
 # Plot the graph
 plt.plot(x_values, y_values, marker='o')
-plt.title('Graph of (2n+1)^2')
+plt.title('Graph of (2n-1)^2')
 plt.xlabel('n')
-plt.ylabel('(2n+1)^2')
+plt.ylabel('(2n-1)^2')
 plt.grid(True)
 
 # Save the plot as a PNG (should be before plt.show())
